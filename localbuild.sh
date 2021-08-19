c68b70e41d1399e3baaed5a007a636b3eda272a7
Barry Smith <67049734+bjs-code@users.noreply.github.com>
2021-08-19 14:44:42 +0100

    Add custom domain name

--- localbuild.sh
+++ localbuild.sh
@@ -13,3 +13,7 @@ mkdir _site
 # Step 2: Jekyll build to resolve "{ % include }" directives
 echo "Compiling static files with JEKYLL"
 bundle exec jekyll build
+
+# Step 3: Place CNAME
+echo "Setting custom domain name"
+echo "doctest.frdy.io" > _site/CNAME # Update with preview.developer.concur.com when going live
